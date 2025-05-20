9cd434c9bdac2c39fb3ce53a33bbc350007644c3
Rana Abu Shammalh <140753564+RanaIbrahem@users.noreply.github.com>
2025-05-20 11:43:09 +0200

    Done

---
+++ SiteDeSoinsDeLaPeau/tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./puplic1/**/*.{html,js}"],
+  theme: {
+   
+      
+    fontFamily: {
+      'playfair': [ 'Playfair Display', 'serif'],
+     'pla': ['Playfair Display SC', 'serif']
+     },
+    extend: {
+      width: {
+        '35px': '300px',
+      },
+      colors:{
+        'primColor': '#dd5e2d',
+        'regal-blue': '#d84811',
+      'gry':'#fcfcfc',
+      'orang':'#ffd7b8',
+      'orangcle':'#ff933f',
+      'bgColor': '#fafafa',
+      'gray':'#8b8b8b', 
+      'bordergray':'#ebebeb',
+      'gain':'gainsboro',
+      'gray2':'#767575',
+      'whit':'#dbd8d8',
+     'blakred':'#5c0000',
+      },
+      
+
+    },
+  },
+  plugins: [],
+}
+ 
+// #ebebeb
+// #ebebeb
+// #dbd8d8
+// #dd5e2d
+// #e6e6e6
+        
+
+
+
+
